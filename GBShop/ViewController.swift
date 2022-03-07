7a77adebc07d13b6195c44ba9c7b39232ede36e4
ArturDokhno <artur.dohno@icloud.com>
2022-03-07 16:59:21 +0500

    Initial Commit

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Артур Дохно on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
