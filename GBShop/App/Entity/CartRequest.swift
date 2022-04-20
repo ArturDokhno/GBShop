b5268526b875d4feecf9dc3127ff9978c2ddadde
ArturDokhno <artur.dohno@icloud.com>
2022-04-20 11:54:15 +0500

    Добавление моделей корзины с запросом и ответом

---
+++ GBShop/App/Entity/CartRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CartRequest.swift
+//  GBShop
+//
+//  Created by Артур Дохно on 20.04.2022.
+//
+
+import Foundation
+
+struct CartRequest {
+    var productId: Int
+    var quantity: Int
+}
